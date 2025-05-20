8d6fb7057eeb129d80a25ce0fa6d7e130eb650e9
saket <saket@saketgupta.com>
2025-05-20 23:59:30 +0530

    Refactor add-task page layout for improved styling and structure

--- src/app/add-task/page.jsx
+++ src/app/add-task/page.jsx
@@ -2,8 +2,10 @@ import React from 'react'
 
 const page = () => {
   return (
-    <div>
-        <h1>This is the place where we can add a new task</h1>
+    <div className='flex justify-center'>
+      <div className='border-2 border-white bg-amber-800'>
+          <h1>This is the place where we can add a new task</h1>
+      </div>
     </div>
   )
 }
